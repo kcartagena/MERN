adb7ae081a8aac1c44d19d03dcc943e6aad110f3
Katie Cartagena <katiecartagena@hotmail.com>
2020-07-19 16:55:36 -0500

    MERN Stack Complete

---
+++ pages/create.js
@@ -0,0 +1,146 @@
+import axios from "axios";
+import React from "react";
+import {
+  Button,
+  Form,
+  Header,
+  Icon,
+  Image,
+  Input,
+  Message,
+  TextArea,
+} from "semantic-ui-react";
+import baseUrl from "../utils/baseUrl";
+import catchErrors from "../utils/catchErrors";
+
+const INITIAL_PRODUCT = {
+  name: "",
+  price: "",
+  media: "",
+  description: "",
+};
+
+function CreateProduct() {
+  const [product, setProduct] = React.useState(INITIAL_PRODUCT);
+  //const [mediaPreview, setMediaPreview] = React.useState("");
+  const [success, setSuccess] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [disabled, setDisabled] = React.useState(true);
+  const [error, setError] = React.useState("");
+
+  React.useEffect(() => {
+    const isProduct = Object.values(product).every((el) => Boolean(el));
+    isProduct ? setDisabled(false) : setDisabled(true);
+  }, [product]);
+
+  function handleChange(event) {
+    const { name, value, files } = event.target;
+    if (name === "media") {
+      setProduct((prevState) => ({ ...prevState, media: files[0] }));
+      setMediaPreview(window.URL.createObjectURL(files[0]));
+    } else {
+      setProduct((prevState) => ({ ...prevState, [name]: value }));
+    }
+  }
+
+  /* async function handleImageUpload() {
+    const data = new FormData();
+    data.append("file", product.media);
+    data.append("upload_preset", "reactreserve");
+    data.append("cloud_name", "<FIND NAME IN DASHBOARD");
+    const response = await axios.post(process.env.CLOUDINARY_URL, data);
+    const mediaUrl = response.data.url;
+    return mediaUrl;
+  } */
+
+  async function handleSubmit(event) {
+    try {
+      event.preventDefault();
+      setLoading(true);
+      setError("");
+      /* const mediaUrl = await handleImageUpload(); */
+      const url = `${baseUrl}/api/product`;
+      const { name, price, description } = product;
+      const payload = { name, price, description /* , mediaUrl */ };
+      const response = await axios.post(url, payload);
+      setProduct(INITIAL_PRODUCT);
+      setSuccess(true);
+    } catch (error) {
+      catchErrors(error, setError);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <Header as="h2" block>
+        <Icon name="add" cor="orange" />
+        Create New Product
+      </Header>
+      <Form
+        loading={loading}
+        error={Boolean(error)}
+        success={success}
+        onSubmit={handleSubmit}
+      >
+        <Message error header="Oops!" content={error} />
+        <Message
+          success
+          icon="check"
+          header="Success!"
+          content="Your product has been posted"
+        />
+        <Form.Group widths="equal">
+          <Form.Field
+            control={Input}
+            name="name"
+            label="Name"
+            placeholder="Name"
+            value={product.name}
+            onChange={handleChange}
+          />
+          <Form.Field
+            control={Input}
+            name="price"
+            label="Price"
+            placeholder="Price"
+            minmin="0.00"
+            step="0.01"
+            type="number"
+            value={product.price}
+            onChange={handleChange}
+          />
+          <Form.Field
+            control={Input}
+            name="media"
+            type="file"
+            label="Media"
+            content="Select Image"
+            accept="image/*"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Image src={mediaPreview} rounded centered size="small" />
+        <Form.Field
+          control={TextArea}
+          name="description"
+          label="Description"
+          placeholder="Description"
+          value={product.description}
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={Button}
+          disabled={disabled || loading}
+          color="blue"
+          icon="pencil alternate"
+          content="Submit"
+          type="submit"
+        />
+      </Form>
+    </>
+  );
+}
+
+export default CreateProduct;
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+import ProductList from "../components/Index/ProductList";
+import ProductPagination from "../components/Index/ProductPagination";
+import baseUrl from "../utils/baseUrl";
+
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
+}
+
+//fetch data from server first
+Home.getInitialProps = async (ctx) => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 6;
+  const url = `${baseUrl}/api/products`;
+  const payload = { params: { page, size } };
+  const response = await axios.get(url, payload);
+
+  return response.data;
+};
+
+export default Home;
